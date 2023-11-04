6594009ebf10ff9b4f0df29bf9e332a36a4b3abf
BasicMultilingualPlane <39372773+BasicMultilingualPlane@users.noreply.github.com>
2023-11-04 14:57:01 -0700

    added yaytext
    added yaytext

---
+++ js/yay.js
@@ -0,0 +1,401 @@
+var ts = {
+    t1: [{
+        action: "ac2",
+        ak3: [820]
+    }],
+    t4: [{
+        action: "ac2",
+        ak3: [821]
+    }],
+    t5: [{
+        action: "ac2",
+        ak3: [822]
+    }],
+    t6: [{
+        action: "ac2",
+        ak3: [823]
+    }],
+    t7: [{
+        action: "ac2",
+        ak3: [824]
+    }],
+    t8: [{
+        action: "ac2",
+        ak3: [863]
+    }],
+    t9: [{
+        action: "ac2",
+        ak3: [866]
+    }],
+    ta: [{
+        action: "ac2",
+        ak3: [828]
+    }],
+    tb: [{
+        action: "ac2",
+        ak3: [857]
+    }],
+    tc: [{
+        action: "ac2",
+        ak3: [858]
+    }],
+    td: [{
+        action: "ac2",
+        ak3: [859]
+    }],
+    te: [{
+        action: "ac2",
+        ak3: [784, 776]
+    }],
+    tf: [{
+        action: "acg",
+        akh: {
+            a: 592,
+            b: 113,
+            c: 596,
+            d: 112,
+            e: 477,
+            f: 607,
+            g: 387,
+            h: 613,
+            i: 305,
+            j: 638,
+            k: 670,
+            l: 1503,
+            m: 623,
+            n: 117,
+            p: 100,
+            q: 98,
+            r: 633,
+            t: 647,
+            u: 110,
+            v: 652,
+            w: 653,
+            y: 654,
+            ".": 729,
+            "[": 93,
+            "(": 41,
+            "{": 125,
+            "?": 191,
+            "!": 161,
+            "'": 44,
+            "<": 62,
+            _: 8254,
+            '"': 8222,
+            ";": 1563,
+            "‿": 8256,
+            "⁅": 8262,
+            "∴": 8757,
+            A: 8704,
+            B: 113,
+            C: 1021,
+            D: 5601,
+            E: 398,
+            F: 8498,
+            G: 387,
+            H: 72,
+            I: 73,
+            J: 383,
+            K: 670,
+            L: 741,
+            M: 87,
+            N: 78,
+            O: 79,
+            P: 1280,
+            Q: 8009,
+            R: 7450,
+            S: 83,
+            T: 8869,
+            U: 8745,
+            V: 923,
+            W: 77,
+            X: 88,
+            Y: 654,
+            Z: 90
+        }
+    }, {
+        action: "aci"
+    }],
+    tj: [{
+        action: "acg",
+        akh: {
+            a: 7424,
+            b: 665,
+            c: 7428,
+            d: 7429,
+            e: 7431,
+            f: 1171,
+            g: 610,
+            h: 668,
+            i: 618,
+            j: 7434,
+            k: 7435,
+            l: 671,
+            m: 7437,
+            n: 628,
+            o: 7439,
+            p: 7448,
+            q: 491,
+            r: 640,
+            s: 115,
+            t: 7451,
+            u: 7452,
+            v: 7456,
+            w: 7457,
+            x: 120,
+            y: 655,
+            z: 7458
+        }
+    }],
+    tk: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 56591,
+        ako: 55356
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 56559,
+        ako: 55356
+    }, {
+        action: "acl",
+        akm: [49, 57],
+        akn: 10073
+    }, {
+        action: "acg",
+        akh: {
+            0: 9471
+        }
+    }],
+    tp: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 9333
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 9327
+    }, {
+        action: "acl",
+        akm: [49, 57],
+        akn: 9263
+    }, {
+        action: "acg",
+        akh: {
+            0: 9450
+        }
+    }],
+    tq: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 127183
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 9275
+    }, {
+        action: "acl",
+        akm: [49, 57],
+        akn: 9283
+    }],
+    tr: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 119743
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 119737
+    }, {
+        action: "acl",
+        akm: [48, 57],
+        akn: 120734
+    }],
+    ts: [{
+        action: "acg",
+        akh: {
+            h: 8462
+        }
+    }, {
+        action: "acl",
+        akm: [65, 90],
+        akn: 119795
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 119789
+    }],
+    tt: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 119847
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 119841
+    }],
+    tu: [{
+        action: "acg",
+        akh: {
+            e: 8495,
+            g: 8458,
+            o: 8500,
+            B: 8492,
+            E: 8496,
+            F: 8497,
+            H: 8459,
+            I: 8464,
+            L: 8466,
+            M: 8499,
+            R: 8475
+        }
+    }, {
+        action: "acl",
+        akm: [65, 90],
+        akn: 119899
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 119893
+    }],
+    tv: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 119951
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 119945
+    }],
+    tw: [{
+        action: "acg",
+        akh: {
+            C: 8450,
+            H: 8461,
+            N: 8469,
+            P: 8473,
+            Q: 8474,
+            R: 8477,
+            Z: 8484
+        }
+    }, {
+        action: "acl",
+        akm: [65, 90],
+        akn: 120055
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 120049
+    }, {
+        action: "acl",
+        akm: [48, 57],
+        akn: 120744
+    }],
+    tx: [{
+        action: "acl",
+        akm: [65, 90],
+        akn: 120107
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 120101
+    }],
+    ty: [{
+        action: "acg",
+        akh: {
+            C: 8493,
+            H: 8460,
+            I: 8465,
+            R: 8476,
+            Z: 8488
+        }
+    }, {
+        action: "acl",
+        akm: [65, 90],
+        akn: 120003
+    }, {
+        action: "acl",
+        akm: [97, 122],
+        akn: 119997
+    }]
+}, tfs = {
+        ac2: function (a, t) {
+            return a.split("").map(function (a) {
+                for (var n = 0; n < t.ak3.length; n++) a += String.fromCharCode(t.ak3[n]);
+                return a
+            }).join("")
+        },
+        acz: function (a, t) {
+            return a.split("").map(function (a) {
+                for (var n = 0; n < t.ak3.length; n++) a = String.fromCharCode(t.ak3[n]) + a;
+                return a
+            }).join("")
+        },
+        acl: function (a, t) {
+            return a.split("").map(function (a) {
+                var n = a.charCodeAt(0);
+                return n >= t.akm[0] && n <= t.akm[1] ? t.ako ? String.fromCodePoint(t.ako, n + t.akn) : a = String.fromCodePoint(n + t.akn) : a
+            }).join("")
+        },
+        acg: function (a, t) {
+            return a.split("").map(function (a) {
+                return t.akh[a] ? String.fromCodePoint(t.akh[a]) : a
+            }).join("")
+        },
+        aci: function (a, t) {
+            return a.split("").reverse().join("")
+        }
+    }, $textarea, ok_to_run_demo = !0,
+    init_textarea = function () {
+        $textarea = $(".textarea").focus(function () {
+            ok_to_run_demo = !1, reset_copy_buttons(), $(this).attr("data-touched") ? $(this).select() : ($(this).attr("data-touched", 1).val(""), run_all_transforms())
+        }).on("input", function () {
+            ok_to_run_demo = !1, reset_copy_buttons(), run_all_transforms($(this).val())
+        })
+    }, reset_copy_buttons = function () {
+        $(".copy").text("copy")
+    }, run_demo = function () {
+        for (var a = "Hello text string my old friend.", t = a.split(""), n = 1; n <= t.length; n++)! function (a) {
+            setTimeout(function () {
+                if (ok_to_run_demo) {
+                    var n = t.slice(0, a).join("");
+                    $textarea.val(n), run_all_transforms(n)
+                }
+            }, 5 * a * (a / 3))
+        }(n)
+    }, run_all_transforms = function (a) {
+        $(".transform_text .value_inner").text(""), a && $(".transform_text .value_inner").each(function () {
+            var t = $(this),
+                n = t.attr("data-transform"),
+                o = run_transform(n, a);
+            t.text(o)
+        })
+    }, run_transform = function (a, t) {
+        for (var n = 0; n < ts[a].length; n++) {
+            var o = ts[a][n];
+            o && (t = tfs[o.action](t, o))
+        }
+        return t
+    }, init_buttons = function () {
+        $(".transform_text").each(function () {
+            var a = $(this),
+                t = $(".copy", a),
+                n = ($(".tweet", a), new Clipboard(t.get(0), {
+                    text: function (t) {
+                        ok_to_run_demo = !1;
+                        var n = $(".value_inner", a).text();
+                        return n
+                    }
+                }));
+            n.on("success", function (a) {
+                reset_copy_buttons(), t.text("copied!")
+            }), $(".tweet", $(this)).click(function () {
+                ok_to_run_demo = !1;
+                var t = "https://twitter.com/intent/tweet?text=" + encodeURIComponent($(".value_inner", a).text() + "");
+                window.open(t, "_blank")
+            })
+        })
+    };
+$(function () {
+    init_textarea(), init_buttons(), run_demo()
+});
